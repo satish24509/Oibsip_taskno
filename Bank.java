7dacdd3191364ff9d424f1163dfe95af8f594443
satish24509 <158153569+satish24509@users.noreply.github.com>
2024-10-28 21:31:36 +0530

    Bank.java

--- Bank.java
+++ Bank.java
@@ -1,17 +1,13 @@
 package org.example;
 import java.util.HashMap;
-
 class Bank {
     private HashMap<String, User> users;
-
     public Bank() {
         users = new HashMap<>();
     }
-
     public void addUser(User user) {
         users.put(user.getUserId(), user);
     }
-
     public User getUser(String userId) {
         return users.get(userId);
     }
